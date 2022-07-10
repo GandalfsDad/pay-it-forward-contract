a5dc905b7d73b1376093042a367c64b3a19b048d
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-07-10 17:23:25 +1000

    Update dependencies and hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.10",
+  networks: {
+    "optimistic-kovan": {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    },
+    "localhost": {
+      url: "http://127.0.0.1:8545",
+      gas: 12000000,
+      chainId: 31337,
+      blockGasLimit: 0x1fffffffffffff,
+      allowUnlimitedContractSize: true,
+    }
+  },
+  etherscan: {
+    apiKey: process.env.OP_ETHERSCAN_API_KEY,
+  }
+  
 };
